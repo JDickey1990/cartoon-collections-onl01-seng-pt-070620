6ba880264819368e1bb9c8ff585164981af4fae7
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 14:55:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,11 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|name| name.capitalize.insert(-1,"!")}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese_types)
+  cheese_types = ["cheddar", "gouda","camembert"]
+  cheese_types.include? ["cheddar", "gouda", "camembert"]
 end
