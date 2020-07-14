cb34598d9ecc3f6504e00e2bf35efb8cecdeccd1
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 15:25:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda","camembert"]
-  cheese_types.include? ("cheddar" || "gouda"|| "camembert")
+  cheese_types.include? ("cheddar" "gouda" "camembert")
   if true
     cheese_types.detect
     else false
